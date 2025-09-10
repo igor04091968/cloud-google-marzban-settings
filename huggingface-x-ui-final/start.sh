8edcaf354b1d0ed97725a2b981266437ef1b21ec
Rachkov II <igor04091968@gmail.com>
2025-09-10 17:50:42 +0000

    feat: Final working configuration for x-ui on Hugging Face

--- huggingface-x-ui-final/start.sh
+++ huggingface-x-ui-final/start.sh
@@ -3,13 +3,14 @@
 # Set a writable directory for the x-ui database
 export XUI_DB_FOLDER=/tmp
 
+# Start socat proxy
+socat TCP-LISTEN:9000,fork,reuseaddr TCP:vds1.iri1968.dpdns.org:443 &
+
 # Function to run chisel client in a loop
 run_chisel() {
   while true; do
-    echo "Starting chisel client with NEW, CORRECT configuration..."
-    # Команда теперь указывает на порт панели 2053
-    /usr/local/bin/chisel client -v --auth "cloud:2025" vds1.iri1968.dpdns.org:8443 R:8000:127.0.0.1:2053
-    
+    echo "Starting chisel client..."
+    /usr/local/bin/chisel client -v --auth "cloud:2025" --proxy http://127.0.0.1:9000 R:2023:127.0.0.1:2023
     echo "Chisel client exited. Restarting in 5 seconds..."
     sleep 5
   done
@@ -20,6 +21,9 @@ run_chisel &
 
 # Wait a moment for the background process to start
 sleep 2
+
+# Set x-ui port
+/usr/local/x-ui/x-ui setting -port 2023
 
 # Set webBasePath
 /usr/local/x-ui/x-ui setting -webBasePath /
